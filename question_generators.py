87739a0ce9f3871559370407ef4393893d1ed37c
Henryjjjs <148510667+Henryjjjs@users.noreply.github.com>
2023-10-28 20:49:18 -0500

    Added Percentage, CubeOfSquare, PointSlopeForm

--- question_generators.py
+++ question_generators.py
@@ -26,6 +26,7 @@ class TimeDifferenceGen(QuestionGenerator):
         minutes_difference = int(time_difference.total_seconds() / 60)
         return minutes_difference
 
+
 @generator
 # generate factored form (ax + b)(cx+d) -> x intercepts are -b/a and -d/c; A = ac, B = ad + bc, C = bd
 class QuadraticZerosGen(QuestionGenerator):
@@ -46,11 +47,13 @@ class QuadraticZerosGen(QuestionGenerator):
             except ZeroDivisionError:
                 return "No Solutions"
 
+
 @generator
 # generate (i1, i2), m1 = a/b, m2 = c/d -> Line1: m = m1, b = -m1i1+i2, Line2: m = m2, b = -m2i1 + i2
 class IntersectingLinesGen(QuestionGenerator):
     def generate_value(self):
-        return [random.randint(-50, 50), random.randint(-50, 50), random.randint(-10, 10) / random.randint(1, 10), random.randint(-10, 10) / random.randint(1, 10)]
+        return [random.randint(-50, 50), random.randint(-50, 50), random.randint(-10, 10) / random.randint(1, 10),
+                random.randint(-10, 10) / random.randint(1, 10)]
 
     def generate_text(self, values):
         i1, i2, m1, m2 = values
@@ -61,6 +64,7 @@ class IntersectingLinesGen(QuestionGenerator):
 
     def generate_answer(self, values):
         return f'({values[0]}, {values[1]})'
+
 
 @generator
 class SimpleAdditionSubtractionThreeTerms(QuestionGenerator):
@@ -85,3 +89,61 @@ class RepeatingSummation(QuestionGenerator):
 
     def generate_answer(self, values):
         return int(values[0] * (values[1] * (values[1] + 1) / 2))
+
+
+@generator
+class Percentage(QuestionGenerator):
+    def generate_value(self):
+        return [random.randint(10, 20) * 5, random.randint(1, 70) * 5, random.randint(1, 10) * 5]
+
+    def generate_text(self, values):
+        return f'{values[0]}% of {values[1]} is {values[2]}% of ____'
+
+    def generate_answer(self, values):
+        return int((values[0] * values[1]) / values[2])
+
+
+@generator
+class CubeOfSquare(QuestionGenerator):
+    def generate_value(self):
+        return [random.randint(5, 10)]
+
+    def generate_text(self, values):
+        return f'If the edge of the cube is {values[0]} units, what is the volume of the cube?'
+
+    def generate_answer(self, values):
+        return int(values[0] ** 3)
+
+
+@generator
+class PercentOff(QuestionGenerator):
+
+    def generate_value(self):
+        return [random.randint(1, 25) * 4, random.randint(20, 80) * 5]
+
+    def generate_text(self, values):
+        return (f'Long bought a phone that was on sale for {values[0]}% off the original price. If the original price '
+                f'of the phone was ${values[1]}.00, how much did he save?')
+
+    def generate_answer(self, values):
+        return "{:.2f}".format(float((values[0] * values[1]) / 100))
+
+
+@generator
+class PointSlopeForm(QuestionGenerator):
+    def generate_value(self):
+        return [random.randint(-10, 10), random.randint(-10, 10), random.randint(-5, 5)]
+
+    def generate_text(self, values):
+        return (f'Which is an equation for the line passing through the point ({values[0]}, {values[1]}) and having a '
+                f'slope of {values[2]}?')
+
+    def generate_answer(self, values):
+        answer = (values[0] * -1) * values[2] + values[1] * -1
+        answer = str(answer)
+        if answer == 0:
+            answer = ""
+        if "-" not in answer:
+            answer = "+ " + answer
+        return f'y = {values[2]}x {answer}'
+
